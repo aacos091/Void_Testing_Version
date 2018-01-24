e35835e4119f4c0a6c2bfae644a2ecb2de238158
Alpha180 <Alexander.Artigas001@mymdc.net>
2018-01-24 15:32:52 -0500

    Add files via upload
    Mostly working PC/Android controls with some of the talk functionality implemented

---
+++ _Scripts/Dialouge.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialouge : MonoBehaviour 
+{
+
+	public static bool cook;
+
+	public static bool medic;
+
+	public static bool captain;
+
+	public static bool engineer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void Update () 
+	{
+
+
+	}
+
+	public void OnClick()
+	{
+
+		if (CameraController.cook == true) 
+		{
+
+			Debug.Log ("caaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+		}
+
+		if (CameraController.medic == true) 
+		{
+
+			Debug.Log ("reeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+
+		}
+
+		//Units.dialogue = false;
+
+	}
+
+}

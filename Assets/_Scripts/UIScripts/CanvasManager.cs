3bf072194ba063b1e4a49f5f7433fea9f9de843b
WillT-357 <willt255@gmail.com>
2018-02-01 14:57:03 -0500

    Sound
    Added some sound fx and adjusted some of the lightning, still needs tweaking

--- Assets/_Scripts/UIScripts/CanvasManager.cs
+++ Assets/_Scripts/UIScripts/CanvasManager.cs
@@ -68,6 +68,7 @@ public class CanvasManager : MonoBehaviour {
     }
 	public void disableCanvas()
 	{
+
 		uiParent.SetActive (false);
 
 	}
@@ -76,6 +77,7 @@ public class CanvasManager : MonoBehaviour {
     // target canvas They are named differently so that setting up games UI is easier.
     public void loadCanvas(GameObject targetCanvas)
 	{
+
 		//First, cancels out of any zoom coroutine
 		if (camController == null)
 			camController = CameraController.S;
---
+++ Assets/playSound.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playSound : MonoBehaviour 
+{
+
+	public AudioSource open;
+
+	public AudioSource close;
+
+	public AudioSource click;
+
+	public AudioSource click2;
+
+	public void Open()
+	{
+
+		open.Play ();
+
+	}
+
+	public void Close()
+	{
+
+		close.Play ();
+
+	}
+
+	public void Click()
+	{
+
+		click.Play ();
+
+	}
+
+	public void Click_Two()
+	{
+
+		click2.Play ();
+
+	}
+
+}

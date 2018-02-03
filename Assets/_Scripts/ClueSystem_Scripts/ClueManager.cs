330a7e4c7b2534abe320caf445830b826d9a4c7c
WillT-357 <willt255@gmail.com>
2018-02-03 17:03:38 -0500

    WE POPULATED YARN VARIABLES THRU UNITY!!!
    WE DID IT LADS! Now we just have to access those yarn variables in chat!

--- Assets/_Scripts/ClueSystem_Scripts/ClueManager.cs
+++ Assets/_Scripts/ClueSystem_Scripts/ClueManager.cs
@@ -5,63 +5,65 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using Yarn.Unity;
 
-public class ClueManager : MonoBehaviour {
+public class ClueManager : MonoBehaviour
+{
 
-	// List to hold all of the clues gathered so far
-	public static List<ClueItem> _cluesCollected;
-	public static ClueManager S;			// Singleton of this class;
-	public static List<ClueInfo> _clueInfoOfCollected;
-	public static GameObject[] _builtCluesMenu;
-	public static Dictionary<string, int> _dictClueNameToIndex;	// Dictionary to store which index of _clueInfoOfCollected corresponds to each clueName;
-	public static List<ClueInfo> _cluesToPresent;       // clues that will be presented during accusation
+    // List to hold all of the clues gathered so far
+    public static List<ClueItem> _cluesCollected;
+    public static ClueManager S;            // Singleton of this class;
+    public static List<ClueInfo> _clueInfoOfCollected;
+    public static GameObject[] _builtCluesMenu;
+    public static Dictionary<string, int> _dictClueNameToIndex; // Dictionary to store which index of _clueInfoOfCollected corresponds to each clueName;
+    public static List<ClueInfo> _cluesToPresent;       // clues that will be presented during accusation
     public static ClueInfo currentClue; //The clue that is being investigated
 
-	private int _cluesToSelect;
-	private Text _cluesCollectedText;
-	private LayerMask _clueLayer;
-	private Image clueImage;
-	public GameObject clueEntry;
-	private static List<string> _nameOfCluesToPresent;  // name of the clues to present. Will be used to search corresponding clue information
+    private int _cluesToSelect;
+    private Text _cluesCollectedText;
+    private LayerMask _clueLayer;
+    private Image clueImage;
+    public GameObject clueEntry;
+    private static List<string> _nameOfCluesToPresent;  // name of the clues to present. Will be used to search corresponding clue information
 
-	public GameObject yarnVarRefObj;
-	ExampleVariableStorage yarnVarRef;
+    public GameObject yarnVarRefObj;
+    ExampleVariableStorage yarnVarRef;
     int yarnIndex = 1;
 
     //[SerializeField]
     //private bool touchControlled;
 
     void Awake()
-	{
+    {
 
-		 yarnVarRef = yarnVarRefObj.GetComponent<ExampleVariableStorage>();
+        yarnVarRef = yarnVarRefObj.GetComponent<ExampleVariableStorage>();
 
 
-		// Populate the Singleton with the followint if and else if statements
-		if (S == null)
-		{
-			S = this;
-		}
-		else if (S != null)
-		{
-			Destroy (this);
-		}
+        // Populate the Singleton with the followint if and else if statements
+        if (S == null)
+        {
+            S = this;
+        }
+        else if (S != null)
+        {
+            Destroy(this);
+        }
         //Make sure that the GameObject this is attached to is not deleted on load
-		DontDestroyOnLoad (gameObject);	
+        DontDestroyOnLoad(gameObject);
 
-	}
+    }
 
-	// Use this for initialization
-	void Start () {
-		_cluesCollected = new List<ClueItem> ();
-		_clueInfoOfCollected = new List<ClueInfo> ();
+    // Use this for initialization
+    void Start()
+    {
+        _cluesCollected = new List<ClueItem>();
+        _clueInfoOfCollected = new List<ClueInfo>();
         _cluesToPresent = new List<ClueInfo>();
 
         // Set the LayerMask _clueLayer to the appropriate layer.
-        _clueLayer = 1 << LayerMask.NameToLayer ("Clue");
+        _clueLayer = 1 << LayerMask.NameToLayer("Clue");
 
-		//DELETE
-		//clueEntry = GameObject.FindGameObjectWithTag ("ClueEntry");
-	}
+        //DELETE
+        //clueEntry = GameObject.FindGameObjectWithTag ("ClueEntry");
+    }
 
     public void PopulateYarnVars()
     {
@@ -72,26 +74,38 @@ public class ClueManager : MonoBehaviour {
             {
                 // Set the Clue Name in Yarn
                 string yarnClueVariableName = "Clue" + yarnIndex.ToString();
-                Yarn.Value yarnClueValue = new Yarn.Value(_clueInfoOfCollected[i].clueName);
-                yarnVarRef.SetValue(yarnClueVariableName, yarnClueValue);
+                string yarnClueFoundVariableName = yarnClueVariableName + "Found";
+                foreach (ExampleVariableStorage.DefaultVariable c in yarnVarRef.defaultVariables)
+                {
+                    // Checks for the yarn variable name (Clue#) and sets the appropriate value
+                    if (c.name == yarnClueVariableName)
+                        c.value = _clueInfoOfCollected[i].clueName;
+                    // Checks for the yarn variable name related to Clue#Found and sets it to the appropriate value
+                    if (c.name == yarnClueFoundVariableName)
+                        c.value = "true";
+                }
+               // Yarn.Value yarnClueValue = new Yarn.Value(_clueInfoOfCollected[i].clueName);
+               // yarnClueValue.variableName = yarnClueVariableName;
+                //yarnVarRef.SetValue(yarnClueValue.variableName, yarnClueValue);
 
-                // Set the ClueFound variable in Yarn
-                string yarnClueFound = yarnClueVariableName + "Found";
-                Yarn.Value yarnClueFoundValue = new Yarn.Value("true");
-                yarnVarRef.SetValue(yarnClueFound, yarnClueFoundValue);
+                //// Set the ClueFound variable in Yarn
+                //string yarnClueFound = yarnClueVariableName + "Found";
+                //Yarn.Value yarnClueFoundValue = new Yarn.Value("true");
+                //yarnClueValue.variableName = yarnClueFound;
+                //yarnVarRef.SetValue(yarnClueFoundValue.variableName, yarnClueFoundValue);
 
                 yarnIndex++;
                 _clueInfoOfCollected[i] = new ClueInfo(_clueInfoOfCollected[i], true); // FUCK THIS LINE!!!!!!
-                   
+
             }
         }
     }
 
     [YarnCommand("AddClue")]
-	public void AddToClueLog(string clueName)
-	{
-		//AddClue(ref ClueDatabase.S.GetClueInfo("clueName"));
-	}
+    public void AddToClueLog(string clueName)
+    {
+        //AddClue(ref ClueDatabase.S.GetClueInfo("clueName"));
+    }
 
     //Adds ClueItem and its ClueInfo passed from ClueItemInspector's raycasted ClueItem
     public void AddClue(ref ClueItem hitClue)
@@ -110,6 +124,8 @@ public class ClueManager : MonoBehaviour {
             _clueInfoOfCollected.Add(objectInfo);
 
             print("Last collected clue: " + _clueInfoOfCollected[_clueInfoOfCollected.Count - 1].clueName);
+
+            PopulateYarnVars();
 
             //Call for CanvasManager to enable "Gather Crew" buttons ONLY when enough clues are collected
             //TODO: Make sure this works even if cluesLeftToChoose changes. Might want to make a default int for the clues to get
@@ -145,7 +161,7 @@ public class ClueManager : MonoBehaviour {
 
     void Update()
     {
-       if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.P))
         {
             foreach (ClueItem c in _cluesCollected)
             {

1ee39e769e5ed38c61d76eb50da26097b709e243
GabrielCabrera002 <gabriel.cabrera002@mymdc.net>
2018-02-13 14:05:23 -0500

    Has scrollbar bug
    The one where the testimony scrollbar disables itself when the scene starts.

---
+++ Assets/_Scripts/ScrollbarEnabler.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+
+[System.Serializable]
+public enum ScrollbarType 
+{
+	vertical, 
+	horizontal
+}
+
+
+[RequireComponent(typeof(CanvasGroup))]
+public class ScrollbarEnabler : MonoBehaviour 
+{
+	CanvasGroup cGroup;
+	[SerializeField] ScrollbarType type = ScrollbarType.vertical;
+	[SerializeField] RectTransform viewport;
+	[SerializeField] RectTransform content;
+	public bool isVisible { get; protected set; }
+	bool shouldHideScrollbar 
+	{ 
+		get 
+		{ 
+			if (type == ScrollbarType.vertical)
+				return content.rect.height > viewport.rect.height; 
+			else 
+				return content.rect.width > viewport.rect.width;
+		} 
+	}
+
+	void Awake()
+	{
+		isVisible = true;
+		cGroup = GetComponent<CanvasGroup>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (shouldHideScrollbar && isVisible)
+			HideScrollbar();
+		else if (!shouldHideScrollbar && !isVisible)
+			ShowScrollbar();
+		
+	}
+
+	void HideScrollbar()
+	{
+		cGroup.alpha = 			0;
+		cGroup.interactable = 	false;
+		cGroup.blocksRaycasts = false;
+		isVisible = false;
+	}
+
+	void ShowScrollbar()
+	{
+		cGroup.alpha = 			1;
+		cGroup.interactable = 	true;
+		cGroup.blocksRaycasts = true;
+		isVisible = true;
+	}
+	
+}

d1501d9e5a6095ded4d1b05887a61a99b261c83e
WillT-357 <willt255@gmail.com>
2018-02-17 10:27:19 -0500

    FirstMage Walk and Idle

---
+++ Assets/AnimationController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AnimationController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject unitObj;
+    private NavMeshAgent navMeshAgent;
+    //private 
+
+	// Use this for initialization
+	void Start () {
+        navMeshAgent = unitObj.GetComponent<UnityEngine.AI.NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

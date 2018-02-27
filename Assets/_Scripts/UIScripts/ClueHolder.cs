bfdbfecc24ecd909c05a85eab75ee0ae83d2c043
WillT-357 <willt255@gmail.com>
2018-02-27 17:26:43 -0500

    Clue Stuff, Cook progress,
    Gabe Manual Merge pt 1,
    Cook prelim anim
    
    Added ClueHolder functionality
    Added InspectObject.json to Source Text
    
    Updated Cook rig and put in a test anim
    
    Updated Textbox prefab with new visuals

--- Assets/_Scripts/UIScripts/ClueHolder.cs
+++ Assets/_Scripts/UIScripts/ClueHolder.cs
@@ -5,65 +5,80 @@ using UnityEngine.UI;
 using UnityEngine.Events;
 using TeaspoonTools.TextboxSystem;
 using TeaspoonTools.Utils;
+using Yarn.Unity;
 
-public class ClueHolder : MonoBehaviour 
+public class ClueHolder : MonoBehaviour
 {
-	public ClueItem clue = null;
-	public GameObject textboxPrefab;
-	public DialogueUITest dialogueUI;
-	public Canvas dialogueCanvas;
-	bool clicked = false;
-	string textToDisplay
-	{
-		get 
-		{
-			if (clue == null)
-				return "There is nothing in this " + this.name + ".";
-			else 
-				return "You found a " + clue.name + " in the " + this.name + ".";
-		}
-	}
+    [SerializeField] string nodeToRun = "InspectObject";
+    public ClueItem clue = null;
+    [SerializeField] GameObject textboxPrefab;
+    [SerializeField] DialogueUITest dialogueUI;
+    [SerializeField] DialogueRunner dialogueRunner;
+    [SerializeField] Canvas dialogueCanvas;
+    bool clicked = false;
+    string textToDisplay
+    {
+        get
+        {
+            if (clue == null)
+                return "There is nothing in this " + this.name + ".";
+            else
+                return "You found a " + clue.name + " in the " + this.name + ".";
+        }
+    }
 
-	// Textbox
-	GameObject textbox;
-	TextboxController tbController;
-	bool textboxIsThere = false;
-	bool respondToInput
-	{
-		get 
-		{ 	return 	CameraController.S.IsZoomed &&
-					!GameController.S.gamePaused && 
-				 	!textboxIsThere; 
-		}
-	}
+    // Textbox
+    GameObject textbox;
+    TextboxController tbController;
+    bool textboxIsThere = false;
+    bool respondToInput
+    {
+        get
+        {
+            return CameraController.S.IsZoomed &&
+                    !GameController.S.gamePaused &&
+                     !dialogueRunner.isDialogueRunning;
+        }
+    }
 
-	void Awake()
-	{
-		if (dialogueUI == null)
-			throw new System.NullReferenceException(this.name + " needs a ref to the dialogue UI.");
-		
-		if (textboxPrefab == null)
-			throw new System.NullReferenceException(this.name + " needs a textbox prefab.");
+    void Awake()
+    {
+        // Make sure this has the components it needs
 
-		if (dialogueCanvas == null)
-			throw new System.NullReferenceException(this.name + " needs a ref to the dialogue canvas.");
-	}
-	// Use this for initialization
-	void Start () 
-	{
-		
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		
-	}
+        if (dialogueUI == null)
+            throw new System.NullReferenceException(this.name + " needs a ref to the dialogue UI.");
 
-	void OnMouseUp()
-	{
-		if (respondToInput)
-		{
+        if (dialogueRunner == null)
+            throw new System.NullReferenceException(this.name + " needs a ref to the dialogue runner.");
+
+        if (textboxPrefab == null)
+            throw new System.NullReferenceException(this.name + " needs a textbox prefab.");
+
+        if (dialogueCanvas == null)
+            throw new System.NullReferenceException(this.name + " needs a ref to the dialogue canvas.");
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseUp()
+    {
+        if (respondToInput)
+        {
+            // Run the node, let the HiddenClueInspector handle the rest
+            HiddenClueInspector.S.clueToInspect = clue;
+            dialogueRunner.StartDialogue(nodeToRun);
+
+
+            /*
 			// Create and initialize the textbox
 			Debug.Log(this.name + " displaying textbox.");
 			GameController.S.RequestGamePause();
@@ -74,8 +89,7 @@ public class ClueHolder : MonoBehaviour
 			tbController.ApplyTextSettings(dialogueUI.textSettings);
 
 			// Make sure the textbox shows up
-			//**COMMENTING THIS OUT BECAUSE IT IS THROWING ERRORS
-			//dialogueUI.needsDialogueCanvas.Add(this.gameObject); 
+			dialogueUI.needsDialogueCanvas.Add(this.gameObject); 
 			
 			tbController.rectTransform.SetParent(dialogueCanvas.transform, false);
 			
@@ -90,72 +104,68 @@ public class ClueHolder : MonoBehaviour
 			// right before it's time for it to display the text
 
 			StartCoroutine(DisplayTextDelayed(Time.deltaTime * 2));
-		}
 
-	}
+			*/
+        }
 
-	void OnMouseDown()
-	{
-		if (respondToInput)
-		{
-			clicked = true;
-			Debug.Log("Clicked " + this.name);
-		}
-	}
+    }
 
-	void OnMouseExit()
-	{
-		if (respondToInput)
-		{
-			clicked = false;
-			Debug.Log("Mouse exited " + this.name);
-		}
-	}
+    void OnMouseDown()
+    {
+        if (respondToInput)
+        {
+            clicked = true;
+            Debug.Log("Clicked " + this.name);
+        }
+    }
 
-	void HidePortrait()
-	{
-		Image portrait = tbController.portrait.GetComponent<Image>();
-		portrait.color = new Color(0, 0, 0, 0);
-	}
+    void OnMouseExit()
+    {
+        clicked = false;
+        Debug.Log("Mouse exited " + this.name);
 
-	IEnumerator DisplayTextDelayed(float delay)
-	{
-		// The canvas manager isn't on a canvas, so I need this to wait a frame or two 
-		// before the textbox can display text without an error popping up
+    }
 
-		yield return new WaitForSeconds(delay);
-		tbController.gameObject.SetActive(true);
-		tbController.PlaceOnScreen(new Vector2(0.5f, 0.0f));
-		// ^For some reason this doesn't work when called before the delay
-		
-		tbController.DisplayText(textToDisplay);
-	}
+    void HidePortrait()
+    {
+        Image portrait = tbController.portrait.GetComponent<Image>();
+        portrait.color = new Color(0, 0, 0, 0);
+    }
 
-	void DoneNeedingDialogueCanvas()
-	{
-		//**COMMENTING THIS OUT BECAUSE IT IS THROWING ERRORS
-		//dialogueUI.needsDialogueCanvas.Remove(this.gameObject);
-	}
+    IEnumerator DisplayTextDelayed(float delay)
+    {
+        // The canvas manager isn't on a canvas, so I need this to wait a frame or two 
+        // before the textbox can display text without an error popping up
 
-	void PrepareForTextboxEnd()
-	{
-		// Need to do certain things when the textbox is done displaying text
-		tbController.DoneDisplayingText.AddListener( () => Destroy(tbController.gameObject) );
-		tbController.DoneDisplayingText.AddListener(DoneNeedingDialogueCanvas);
-		tbController.DoneDisplayingText.AddListener( () => textboxIsThere = false);
+        yield return new WaitForSeconds(delay);
+        tbController.gameObject.SetActive(true);
+        tbController.PlaceOnScreen(new Vector2(0.5f, 0.0f));
+        // ^For some reason this doesn't work when called before the delay
 
-		//**COMMENTING THIS OUT BECAUSE IT IS THROWING ERRORS.
-		//tbController.DoneDisplayingText.AddListener( () => CameraController.S.SetPanning(false) );
+        tbController.DisplayText(textToDisplay);
+    }
 
-		if (clue != null)
-			tbController.DoneDisplayingText.AddListener(ShowClueInInspectionWindow);
-	}
+    void DoneNeedingDialogueCanvas()
+    {
+        dialogueUI.needsDialogueCanvas.Remove(this.gameObject);
+    }
 
-	void ShowClueInInspectionWindow()
-	{
+    void PrepareForTextboxEnd()
+    {
+        // Need to do certain things when the textbox is done displaying text
+        tbController.DoneDisplayingText.AddListener(() => Destroy(tbController.gameObject));
+        tbController.DoneDisplayingText.AddListener(DoneNeedingDialogueCanvas);
+        tbController.DoneDisplayingText.AddListener(() => textboxIsThere = false);
+        tbController.DoneDisplayingText.AddListener(() => CameraController.S.SetPanning(false));
+        HiddenClueInspector.S.clueToInspect = clue;
 
-		//**COMMENTING THIS OUT BECAUSE IT IS THROWING ERRORS.
-		//ClueItemInspector.S.ShowClueInUI(clue.gameObject);
-	}
-	
+        if (clue != null)
+            tbController.DoneDisplayingText.AddListener(ShowClueInInspectionWindow);
+    }
+
+    void ShowClueInInspectionWindow()
+    {
+        ClueItemInspector.S.ShowClueInUI(clue.gameObject);
+    }
+
 }

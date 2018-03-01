fff51e26fb17346570dc352fd172a33338e51665
Alpha180 <Alexander.Artigas001@mymdc.net>
2018-02-28 23:35:08 -0500

    FMOD
    Added Stages to the theme and added ambient ship sounds as well as the engine room audio

---
+++ Assets/EngineRoom.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineRoom : MonoBehaviour {
+	private void OnTriggerEnter(Collider other)
+	{
+		
+		Debug.Log("In Engine room");
+		
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		
+		Debug.Log("Leaving engine room");
+		
+	}
+}

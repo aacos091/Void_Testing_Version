67ba449b30738d2f00ec6d055a1db55605e7948e
Alpha180 <Alexander.Artigas001@mymdc.net>
2018-03-01 14:04:08 -0500

    FMOD
    Added in footstep sounds and setup for the rest of the ship ambiance.

---
+++ Assets/music_Collider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class music_Collider : MonoBehaviour 
+{
+	
+	Quaternion rotate;
+
+	void Start()
+	{
+
+		rotate = transform.rotation;
+	}
+
+	void Update () 
+	{
+
+		transform.rotation = rotate;
+		
+	}
+
+}
